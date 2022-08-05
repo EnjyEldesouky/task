2dd1b96b6a6b6952cde13af49597d0ceeb85b185
Enjy <enjyeldesouky77@gmail.com>
2022-08-05 19:16:54 +0200

    result now is 60

--- taskk.kt
+++ taskk.kt
@@ -1,8 +1,8 @@
 fun main()
 {
     var x = 20
-    var y = 20
+    var y = 40
     var result = var x + var y
     println(result)
-   // i have updated variable x
+   // i have updated variable y
 }
